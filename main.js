5c8095061817e2783805585b3b07557fdbc2b827
sasa-us <sharryluh@gmail.com>
2018-12-26 16:01:50 -0800

    read dummy file to console

--- main.js
+++ main.js
@@ -6,5 +6,16 @@ function getAll() {
 }
 
 function getAllfromFile() { 
-    // alert('hi');
-}
+    $.ajax({
+        url: 'dummy.json',
+        dataType: 'text',
+        method: 'get',
+        success: function(response){
+           console.log('response is ', response);
+           insertAlltoDB(response);
+        },
+        error: function( ){
+            console.log('server not response'); 
+        }
+    });
+}//end getAllfromFile()
